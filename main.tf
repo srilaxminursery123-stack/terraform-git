0ef54ffa96e9015a87a9f4ec909e66516ddd2625
srilaxminursery123-stack <srilaxminursery123@gmail.com>
2026-09-22 09:51:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,14 @@ provider "google" {
   region  = "asia-south1"
 }
 
-resource "google_storage_bucket" "terraform_test" {
-  name     = "project-e224783c-d3b2-4cda-bea-terraform-test"
-  location = "asia-south1"
+resource "google_storage_bucket" "terraform_demo" {
+  name     = "terraform-demo-project-e224783c-d3b2"
+  location = "ASIA-SOUTH1"
+
+  uniform_bucket_level_access = true
+
+  labels = {
+    environment = "demo"
+    managed_by  = "terraform"
+  }
 }
